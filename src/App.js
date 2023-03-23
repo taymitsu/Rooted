178576d358f006e91e130fd4e1963344ab0c64c3
taymitsu <tmitsumori96@gmail.com>
2023-03-23 13:57:28 -0700

    gh-pages installed, package.json updated

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ function App() {
           <Route exact path="/create-account" component={CreateAccount} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/test" component={TestComponent} />
+          <Route exact path="/test" component={TestComponent} />
+          <Route exact path="/test" component={TestComponent} />
+          <Route exact path="/test" component={TestComponent} />
         </Switch>
       </div>
     </BrowserRouter>
